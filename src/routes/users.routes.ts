589111212ad43ad359b7635cc642e421a9275663
unknown <flaviomartins_silva@hotmail.com>
2020-05-01 20:33:17 -0300

    upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,10 @@
-import { Router } from 'express';
+import { Router, request, response } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -14,5 +18,14 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;
