d2e769c249e0805fbb40baaaafee45c458a9fb5a
unknown <flaviomartins_silva@hotmail.com>
2020-05-01 18:23:27 -0300

    criptografia da senha

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,8 +6,9 @@ usersRouter.post('/', async (request, response) => {
   try {
     const { name, email, password } = request.body;
     const createUser = new CreateUserService();
-
     const user = await createUser.execute({ name, email, password });
+    delete user.password;
+
     return response.json(user);
   } catch (error) {
     return response.status(400).json({ error: error.message });
